1e8ad69afafd559ff2ae9f4375a12d1ed6ab210c
Laks0 <laksjoaquin@gmail.com>
2020-02-24 17:01:13 -0300

    First commit

---
+++ bot.py
@@ -0,0 +1,30 @@
+import wikipedia
+import random
+from datetime import date
+
+wikipedia.set_lang("es")
+
+today = date.today()
+
+months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+
+fecha = today.strftime("%d") + " de " + months[today.month-1]
+pag = wikipedia.page(fecha)
+content = pag.content
+
+table = content.split("\n")
+
+nAcont = 0
+fAcont = 0
+for i, a in enumerate(table):
+	if a == "== Acontecimientos ==":
+		nAcont = i
+
+	if nAcont != 0 and a == "":
+		fAcont = i
+		break
+
+
+rAcont = table[random.randint(nAcont + 1, fAcont)].split(":")
+txt = "Un día como hoy en " + rAcont[0] + rAcont[1]
+print(txt)
